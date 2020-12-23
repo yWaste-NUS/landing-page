27b7e39ed988e560a9b0aa05ed159c28ac09dc45
nwjbrandon <nwjbrandon@gmail.com>
2020-12-23 22:19:21 +0800

    feat: add partners

--- src/App.tsx
+++ src/App.tsx
@@ -4,51 +4,63 @@ import Showcase from 'assets/Showcase.svg';
 import ScanReceipt from 'assets/ScanReceipt.png';
 import Tracking from 'assets/Tracking.svg';
 import Reminder from 'assets/Reminder.png';
+import NUS from 'assets/NUS.jpg';
 import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="app">
-      <div className="app__intro">
-        <img className="app__logo" src={AppLogo} alt="AppLogo" />
-        <div className="app__title">YWaste</div>
-        <div className="app__subtitle">
-          A Simple Solution To Managing Food Waste
+    <div>
+      <div className="app">
+        <div className="app__intro">
+          <img className="app__logo" src={AppLogo} alt="AppLogo" />
+          <div className="app__title">YWaste</div>
+          <div className="app__subtitle">
+            A Simple Solution To Managing Food Waste
+          </div>
+          <button className="app__trynow">Try Now</button>
+          <img className="app__showcase" src={Showcase} alt="Showcase" />
         </div>
-        <button className="app__trynow">Try Now</button>
-        <img className="app__showcase" src={Showcase} alt="Showcase" />
+        <div className="app__sellingpoints">
+          <div className="app__sellingpoint">
+            <img
+              className="app__sellingpoint--icon"
+              src={ScanReceipt}
+              alt="ScanReceipt"
+            />
+            <div className="app__sellingpoint--desc">
+              Scan to input details of purchased items into app
+            </div>
+          </div>
+          <div className="app__sellingpoint">
+            <img
+              className="app__sellingpoint--icon"
+              src={Reminder}
+              alt="Reminder"
+            />
+            <div className="app__sellingpoint--desc">
+              Receive reminders of expiring food
+            </div>
+          </div>
+          <div className="app__sellingpoint">
+            <img
+              className="app__sellingpoint--icon"
+              src={Tracking}
+              alt="Tracking"
+            />
+            <div className="app__sellingpoint--desc">
+              Track food inventory conveniently
+            </div>
+          </div>
+        </div>
+        <div>
+          <hr className="app__divider" />
+          <img className="app__partners" src={NUS} alt="NUS" />
+          <hr className="app__divider" />
+        </div>
       </div>
-      <div className="app__sellingpoints">
-        <div className="app__sellingpoint">
-          <img
-            className="app__sellingpoint--icon"
-            src={ScanReceipt}
-            alt="ScanReceipt"
-          />
-          <div className="app__sellingpoint--desc">
-            Scan to input details of purchased items into app
-          </div>
-        </div>
-        <div className="app__sellingpoint">
-          <img
-            className="app__sellingpoint--icon"
-            src={Reminder}
-            alt="Reminder"
-          />
-          <div className="app__sellingpoint--desc">
-            Receive reminders of expiring food
-          </div>
-        </div>
-        <div className="app__sellingpoint">
-          <img
-            className="app__sellingpoint--icon"
-            src={Tracking}
-            alt="Tracking"
-          />
-          <div className="app__sellingpoint--desc">
-            Track food inventory conveniently
-          </div>
-        </div>
+      <div className="app__footer">
+        <div className="app__footer--left">Terms of Service</div>
+        <div className="app__footer--right">Privacy Policy</div>
       </div>
     </div>
   );
