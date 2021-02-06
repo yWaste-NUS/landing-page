350879c4ece998205951a60f00a4e18944d1f619
namiwa <khairul.nasri895@gmail.com>
2021-02-06 09:35:06 -0800

    init dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,12 @@
-import React from 'react';
-import AppLogo from 'assets/RGBLogo.png';
-import Showcase from 'assets/Showcase.svg';
-import ScanReceipt from 'assets/ScanReceipt.png';
-import Tracking from 'assets/Tracking.svg';
-import Reminder from 'assets/Reminder.png';
-import NUS from 'assets/NUS.jpg';
-import './App.scss';
+import * as React from 'react';
 
 const App: React.FC = () => {
   return (
     <div>
-      <div className="app">
-        <div className="app__intro">
-          <img className="app__logo" src={AppLogo} alt="AppLogo" />
-          <div className="app__title">YWaste</div>
-          <div className="app__subtitle">
-            A Simple Solution To Managing Food Waste
-          </div>
-          <button className="app__trynow">Try Now</button>
-          <img className="app__showcase" src={Showcase} alt="Showcase" />
-        </div>
-        <div className="app__sellingpoints">
-          <div className="app__sellingpoint">
-            <img
-              className="app__sellingpoint--icon"
-              src={ScanReceipt}
-              alt="ScanReceipt"
-            />
-            <div className="app__sellingpoint--desc">
-              Scan to input details of purchased items into app
-            </div>
-          </div>
-          <div className="app__sellingpoint">
-            <img
-              className="app__sellingpoint--icon"
-              src={Reminder}
-              alt="Reminder"
-            />
-            <div className="app__sellingpoint--desc">
-              Receive reminders of expiring food
-            </div>
-          </div>
-          <div className="app__sellingpoint">
-            <img
-              className="app__sellingpoint--icon"
-              src={Tracking}
-              alt="Tracking"
-            />
-            <div className="app__sellingpoint--desc">
-              Track food inventory conveniently
-            </div>
-          </div>
-        </div>
-        <div>
-          <hr className="app__divider" />
-          <img className="app__partners" src={NUS} alt="NUS" />
-          <hr className="app__divider" />
-        </div>
-      </div>
-      <div className="app__footer">
-        <div className="app__footer--left">Terms of Service</div>
-        <div className="app__footer--right">Privacy Policy</div>
+      <h2> yWaste: fudprints </h2>
+      <h4> Your solution to better manage food invoices. </h4>
+      <div>
+        <p>Try out our invoice to text parser on telegram here!</p>
       </div>
     </div>
   );
